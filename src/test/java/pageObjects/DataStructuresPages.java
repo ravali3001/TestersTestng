701fa42e4908389b71f66b864216a233f71b049b
Ravali Veerla <ravaliveerla@Ravalis-MacBook-Pro.local>
2026-04-04 17:31:36 -0500

    Resolve conflict and remove target files

---
+++ src/test/java/pageObjects/DataStructuresPages.java
@@ -0,0 +1,155 @@
+package pageObjects;
+
+import java.time.Duration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DataStructuresPages {
+	 WebDriver driver;
+	 WebDriverWait wait;
+	 private static final Logger logger = LogManager.getLogger(DataStructuresPages.class); 
+
+	 
+	 public DataStructuresPages(WebDriver driver) { 
+	      this.driver = driver;
+	      this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	 }
+	
+	By DsIntro = By.xpath("//a[@href='data-structures-introduction' and normalize-space()='Get Started']");
+	By Timecomplexity =By.xpath("//a[@href='time-complexity']");
+	By practice=By.xpath("//a[@href='/data-structures-introduction/practice']");
+	By TryHereDS=By.xpath("//a[@href='/tryEditor']");
+	By TryHere=By.xpath("//a[contains(text(),'Try here')]");
+	By TRYhereDS=By.xpath("//a[text()='Try here>>>']");
+	By editor1=By.xpath("//pre[@class=' CodeMirror-line ']");
+	private By editorDS = By.xpath("//div[contains(@class,'CodeMirror')]//textarea");
+	private By ClickRunBtn=By.xpath("//button[@type='button']");
+	private By output = By.xpath("//pre[@id='output']");
+	private By DStitle=By.xpath("//h4[text()='Data Structures-Introduction']");
+	private By DSlogo=By.xpath("//a[@href='/']");
+	private By DSlink=By.xpath("/html/body/div[1]/nav/div/div[2]/ul/a[2]");
+	private By DSdrop1=By.xpath("//a[@href='#']");
+	private By DSbutton=By.xpath("//a[@href='/logout']");
+
+	
+
+	
+	public String isTitleDisplayed() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(DStitle)).getText();
+    }
+	
+	public String islogoDisplayed() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(DSlogo)).getText();
+    }
+	public String islinkDisplayed() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(DSlink)).getText();
+    }
+	public String isbuttonDisplayed() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(DSbutton)).getText();
+    }
+	public String isdropdownDisplayed() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(DSdrop1)).getText();
+    }
+	public void DSClickIntro() {
+		driver.findElement(DsIntro).click();
+		
+	}
+	public void DSClickTimeComplexity() {
+		driver.findElement(Timecomplexity).click();
+		
+	}
+
+	public void DSClickPracticeQ() {
+		driver.findElement(Timecomplexity).click();
+
+		driver.findElement(practice).click();
+	}
+	public void DSClickTryhere() {
+	WebElement txt2=driver.findElement(TryHere);
+    	//JavascriptExecutor js = (JavascriptExecutor) driver;
+    	//js.executeScript("arguments[0].scrollIntoView(true);", txt2);
+	    //txt2.click();
+		//driver.findElement(TryHereDS).click();
+	Actions actions = new Actions(driver);
+	actions.moveToElement(txt2).perform();
+	txt2.click();
+	}
+	public void enterCode(String Code) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(editor1));
+	    JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript(
+            "document.querySelector('.CodeMirror').CodeMirror.setValue(arguments[0]);",
+            Code);
+		
+}
+	public void clickRunButton() {
+       driver.findElement(ClickRunBtn).click();
+    }
+
+	public String getResult() {
+
+	    try {
+	        Alert alert = driver.switchTo().alert();
+	        String alertText = alert.getText();
+	        logger.info("Alert Text: " + alertText);  // print
+	        alert.accept();
+	        return alertText;
+
+	    } catch (NoAlertPresentException e) {
+	    	String outputText = driver.findElement(output).getText();  // get text
+	    	 logger.info("Output Text: " + outputText);          // print text
+	        return outputText;
+	       
+	    }
+	}
+     public String getOutput() {
+    	WebElement txt3=driver.findElement(output);
+    	
+	    String result = "";  
+	        try {
+	            Alert alert = driver.switchTo().alert();
+	            result = alert.getText();
+	            logger.info("*******Alert Text******: " + result);
+	            alert.accept();
+	        } catch (NoAlertPresentException e) {
+	           
+	            result = driver.findElement(output).getText();  
+	            logger.info("******Editor Output*****: " + result);
+	        }
+
+	        return result;
+	    
+	    }
+    
+    public String getPageTitle() {
+    	return driver.getTitle();
+    }
+    public boolean isTextDisplayed(String text) {
+        return driver.findElement(DStitle).isDisplayed();
+    }
+}
+
+
+
+
---
+++ src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,114 @@
+package pageObjects;
+
+
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.support.ui.WebDriverWait;
+	import org.openqa.selenium.support.ui.ExpectedConditions;
+	import java.time.Duration;
+	import java.util.Map;
+
+	import factory.DriverFactory;
+	import utilities.ConfigReader;
+	import utilities.ExcelReader;
+
+	public class LoginPage {
+
+	    WebDriver driver;
+	    ConfigReader configReader;
+	    static Map<String, Map<String, String>> testData;
+	    
+	  
+	    public LoginPage() {
+	    	this.driver = DriverFactory.getDriver();
+	       
+	    	ConfigReader.loadConfig();
+	    }
+
+	    // Locators
+	    private By getStartedBtn = By.linkText("Get Started");
+	    private By signInLink = By.linkText("Sign in");
+	    private By usernameField = By.id("id_username");
+	    private By passwordField = By.id("id_password");
+	    private By loginBtn = By.xpath("//input[@value='Login']");
+	    private By logoutLink = By.linkText("Sign out");
+	    private By errorMsg = By.xpath("//div[contains(@class,'alert')]");
+
+	    // Actions
+
+	    /** Open DS Algo portal */
+	    public void openPortal() {
+	        driver.get(ConfigReader.getProperty("url"));
+	    }
+
+	    /** Click Get Started button on homepage */
+	    public void clickGetStarted() {
+	        driver.findElement(getStartedBtn).click();
+	    }
+
+	    /** Click Sign in link */
+	    public void clickSignIn() {
+	        driver.findElement(signInLink).click();
+	    }
+
+	    /** Verify Login page is displayed */
+	    public boolean isLoginPageDisplayed() {
+	        return driver.getTitle().contains("Login");
+	    }
+
+	    /** Enter username */
+	    public void enterUsername(String username) {
+	        driver.findElement(usernameField).clear();
+	        driver.findElement(usernameField).sendKeys(username);
+	    }
+
+	    /** Enter password */
+	    public void enterPassword(String password) {
+	        driver.findElement(passwordField).clear();
+	        driver.findElement(passwordField).sendKeys(password);
+	    }
+
+	    /** Click Login button */
+	    public void clickLogin() {
+	        driver.findElement(loginBtn).click();
+	    }
+
+	    /** Verify login is successful */
+	    public boolean isLoginSuccessful() {
+	        return driver.findElements(logoutLink).size() > 0;
+	    }
+
+	    /** Verify login error is displayed */
+	    public boolean isLoginErrorDisplayed() {
+	        return driver.findElements(errorMsg).size() > 0;
+	    }
+	    
+	    public void successfulLogin() {
+	    	if (testData == null) {
+	      //      testData = ExcelReader.getLoginData();
+	        }
+	    	Map<String, String> validCreds = testData.get("Login_Valid");
+		    enterUsername(validCreds.get("Username"));
+		    enterPassword(validCreds.get("Password"));
+		    clickLogin();
+	    }
+	    
+	    public void waitForSuccessfulLogin() {
+	        new WebDriverWait(driver, Duration.ofSeconds(5))
+	                .until(ExpectedConditions.urlContains("home"));
+	    }
+	    
+	      // Check whether Login button is enabled
+	    public boolean isLoginButtonEnabled() {
+	        return driver.findElement(loginBtn).isEnabled();
+	    }
+
+	        //  Check password field masking
+	    public String getPasswordFieldType() {
+	        return driver.findElement(passwordField).getAttribute("type");
+	    }
+
+	}
+
+
